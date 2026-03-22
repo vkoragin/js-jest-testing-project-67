6c8610607e51162041d16bc40bf98650cb2122e5
Victor Koriagin <superiaschik@rambler.ru>
2026-03-23 02:40:19 +0300

    test fix try

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,24 @@
-import js from '@eslint/js';
+import js from "@eslint/js";
 
 export default [
   js.configs.recommended,
   {
-    files: ['**/*.js'],
+    files: ["**/*.js"],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: {
-        process: 'readonly',
-        console: 'readonly',
-        test: 'readonly',
-        expect: 'readonly',
-        beforeEach: 'readonly',
-        afterEach: 'readonly',
-        describe: 'readonly',
-        jest: 'readonly',
+        process: "readonly",
+        console: "readonly",
+        test: "readonly",
+        expect: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+        describe: "readonly",
       },
     },
     rules: {
-      'no-console': 'off',
-      // Правила стиля
-      quotes: ['error', 'single'], // одинарные кавычки
-      semi: ['error', 'always'], // точка с запятой в конце
-      'comma-dangle': ['error', 'always-multiline'], // запятая в конце строк в многострочных объектах
-      'arrow-parens': ['error', 'always'], // скобки у стрелочных функций
-      'brace-style': ['error', '1tbs'], // стиль фигурных скобок
-      'no-extra-semi': 'error', // лишние точки с запятой
+      "no-console": "off",
     },
   },
 ];
