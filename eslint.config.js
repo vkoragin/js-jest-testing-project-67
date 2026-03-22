c7035edc26125088b5b541b3ae29265ec8c3d89a
Victor Koriagin <superiaschik@rambler.ru>
2026-03-23 02:45:43 +0300

    test fix try

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,32 @@
-import js from "@eslint/js";
+import js from '@eslint/js';
 
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.js"],
+    files: ['**/*.js'],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: {
-        process: "readonly",
-        console: "readonly",
-        test: "readonly",
-        expect: "readonly",
-        beforeEach: "readonly",
-        afterEach: "readonly",
-        describe: "readonly",
+        process: 'readonly',
+        console: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        describe: 'readonly',
+        jest: 'readonly',
       },
     },
     rules: {
-      "no-console": "off",
+      'no-console': 'off',
+      // Правила стиля
+      quotes: ['error', 'single'], // одинарные кавычки
+      semi: ['error', 'always'], // точка с запятой в конце
+      'comma-dangle': ['error', 'always-multiline'], // запятая в конце строк в многострочных объектах
+      'arrow-parens': ['error', 'always'], // скобки у стрелочных функций
+      'brace-style': ['error', '1tbs'], // стиль фигурных скобок
+      'no-extra-semi': 'error', // лишние точки с запятой
     },
   },
 ];
