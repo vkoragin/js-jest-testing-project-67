34003b6e2d394bed708abfca2a05f21ab87ea8ef
Victor Koriagin <superiaschik@rambler.ru>
2026-03-23 04:06:17 +0300

    test fix try

--- eslint.config.js
+++ eslint.config.js
@@ -3,47 +3,36 @@ import js from '@eslint/js'
 export default [
   js.configs.recommended,
   {
-    'files': ['**/*.js'],
-    'languageOptions': {
-      'ecmaVersion': 'latest',
-      'sourceType': 'module',
-      'globals': {
-        'process': 'readonly',
-        'console': 'readonly',
-        'test': 'readonly',
-        'beforeEach': 'readonly',
-        'afterEach': 'readonly',
-        'describe': 'readonly',
-        'jest': 'readonly',
-      },
-    },
-    'rules': {
-      'no-console': 'off',
-
-      // Отступы
-      'indent': ['error', 2],
-
-      // Кавычки для строк
-      'quotes': ['error', 'single'],
-
-      // Точки с запятой - не используем
-      'semi': ['error', 'never'],
-      'no-extra-semi': 'error',
-
-      // Стрелочные функции
-      'arrow-parens': 'off',
-
-      // Фигурные скобки
-      'brace-style': ['error', 'stroustrup'],
-
-      // Запятые в конце
-      'comma-dangle': ['error', 'always-multiline'],
+    files: ['eslint.config.js'], // специальные правила для конфига
+    rules: {
+      'quote-props': ['error', 'always'], // всегда с кавычками
     },
   },
   {
-    'files': ['eslint.config.js'], // только для файла конфигурации
-    'rules': {
-      'quote-props': ['error', 'always'], // всегда с кавычками
+    files: ['**/*.js', '!eslint.config.js'], // для всех остальных файлов, исключая конфиг
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        process: 'readonly',
+        console: 'readonly',
+        test: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        describe: 'readonly',
+        jest: 'readonly',
+      },
+    },
+    rules: {
+      'no-console': 'off',
+      'quote-props': ['error', 'as-needed'], // кавычки только когда нужны
+      indent: ['error', 2],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      'no-extra-semi': 'error',
+      'arrow-parens': 'off',
+      'brace-style': ['error', 'stroustrup'],
+      'comma-dangle': ['error', 'always-multiline'],
     },
   },
 ]
